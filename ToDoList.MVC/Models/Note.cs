a58c888531288d042ca5289bede48f827ba559c0
Sadeq Rahman <srahman1@ups.com>
2023-12-11 14:17:44 -0500

    updated index file disabled nullable option added Models/Note.cs

---
+++ ToDoList.MVC/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.MVC.Models
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool SoftDelete { get; set; }
+
+    }
+}
