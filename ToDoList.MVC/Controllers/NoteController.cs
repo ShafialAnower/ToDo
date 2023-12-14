9f3ebfd1fc6a9859a2268a0b08bf04a84f4bbc40
ShafialAnower123 <fahimshafial@gmail.com>
2023-12-14 00:16:56 -0500

    -Moved all of style attributes into css folder -Created a NoteController that has the corrosponding view pages based on the ToDo user interface -Added a simple table in the Views/Note folder based on the ToDo interface

---
+++ ToDoList.MVC/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.MVC.Controllers
+{
+    public class NoteController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult ViewTask()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
